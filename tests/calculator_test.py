b547ae3a546286c6ec80455267c9385921c77c7f
Istvan Viczian <viczian.istvan@gmail.com>
2025-06-10 15:54:28 +0200

    Data driven testing külső fájl alapján

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,12 +1,23 @@
 from selenium.webdriver.common.by import By
 import pytest
+from selenium.webdriver.remote.webdriver import WebDriver
 
 # DRY = don't repeat yourself
 # Data driven testing
 
 
-@pytest.mark.parametrize("a,b,expected", [(0, 0, 0), (5, 8, 13), (-5, 8, 3)])
-def test_add(driver, a, b, expected):
+def read_file():
+    data = []
+    with open("tests/calculator.csv") as f:
+        for line in f:
+            parts = line.split(",")
+            data.append((int(parts[0]), int(parts[1]), int(parts[2])))
+    return data
+
+
+# @pytest.mark.parametrize("a,b,expected", [(0, 0, 0), (5, 8, 13), (-5, 8, 3)])
+@pytest.mark.parametrize("a,b,expected", read_file())
+def test_add(driver: WebDriver, a: int, b: int, expected: int):
     print("Test")
     driver.get("http://127.0.0.1:5501/pages/calculator/")
     input_field = driver.find_element(By.ID, "a-input")
