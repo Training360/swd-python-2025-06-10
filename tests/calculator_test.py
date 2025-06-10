f6fffbf0779b5c0d6394927991ba84c325bce1aa
Istvan Viczian <viczian.istvan@gmail.com>
2025-06-10 15:39:39 +0200

    fixture kiemelés külön fájlba, data-driven testing

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,19 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import pytest
+
+# DRY = don't repeat yourself
+# Data driven testing
 
 
-def test_add():
-    options = Options()
-    options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=options)
-
+@pytest.mark.parametrize("a,b,expected", [(0, 0, 0), (5, 8, 13), (-5, 8, 3)])
+def test_add(driver, a, b, expected):
+    print("Test")
     driver.get("http://127.0.0.1:5501/pages/calculator/")
     input_field = driver.find_element(By.ID, "a-input")
-    input_field.send_keys("5")
+    input_field.send_keys(str(a))
 
     input_field = driver.find_element(By.ID, "b-input")
-    input_field.send_keys("8")
+    input_field.send_keys(str(b))
     # When
     button = driver.find_element(By.ID, "submit-button")
     button.click()
@@ -21,4 +21,4 @@ def test_add():
     input_field = driver.find_element(By.ID, "result-input")
     result = input_field.get_attribute("value")
     print("Az eredmeny:", result)
-    assert result == "13"
+    assert result == str(expected)
---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.chrome.options import Options
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver():
+    print("Before")
+    options = Options()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    print("After")
+    driver.quit()
