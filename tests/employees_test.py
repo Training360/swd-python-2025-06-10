7ba4b0baa7901682d085f2f6c3523c4c7db330ce
Istvan Viczian <viczian.istvan@gmail.com>
2025-06-12 15:11:18 +0200

    Adatbázis hozzáférés

--- tests/employees_test.py
+++ tests/employees_test.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from employees_po import EmployeesPage
 import uuid
+import psycopg2 as psycopg
 
 
 def test_save(driver: WebDriver):
@@ -14,3 +15,24 @@ def test_save(driver: WebDriver):
     table = page.get_table()
 
     assert name in [row[1] for row in table]
+
+
+def test_save_db(driver: WebDriver):
+    delete_employees()
+    page = EmployeesPage(driver)
+    page.go()
+    page.set_name()
+    page.save()
+    page.wait_for_message()
+    table = page.get_table()
+
+    assert "John Doe" == table[0][1]
+
+
+def delete_employees():
+    with psycopg.connect(
+        user="employees", password="employees", host="localhost", dbname="employees"
+    ) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("DELETE FROM employees;")
+            conn.commit()
